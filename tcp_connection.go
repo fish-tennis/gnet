bc8f20c9cd37d6f8303d4cf70bbb1233bc2c479a
fish-tennis <93663886@qq.com>
2021-12-30 16:41:25 +0800

    TrySendPacket fix

--- tcp_connection.go
+++ tcp_connection.go
@@ -320,7 +320,12 @@ func (this *TcpConnection) SendPacket(packet Packet) bool {
 // 可以防止某些"不重要的"数据包造成chan阻塞,比如游戏项目常见的聊天广播
 func (this *TcpConnection) TrySendPacket(packet Packet, timeout time.Duration) bool {
 	if timeout == 0 {
-		return this.SendPacket(packet)
+		// 非阻塞方式写chan
+		select {
+		case this.sendPacketCache <- packet:
+			return true
+		}
+		return false
 	}
 	sendTimeout := time.After(timeout)
 	for {
